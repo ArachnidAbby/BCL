91ecc2f742b51f7d352c88cf6311f90f6cee4e7a
Benjamin Austin Jr <spidertyler1122@gmail.com>
2022-12-04 11:49:42 -0500

    print function added (without new line)

--- src/Ast/standardfunctions.py
+++ src/Ast/standardfunctions.py
@@ -12,24 +12,33 @@ fmt = "%i \n\0"
 c_fmt = ir.Constant(ir.ArrayType(ir.IntType(8), len(fmt)),
                     bytearray(fmt.encode("utf8")))
 gpistr = None
+gpistr2 = None
 
 voidptr_ty = ir.IntType(8).as_pointer()
 
 # todo: make this more readable!
 def declare_printf(module):
-    global printf, gpistr
+    global printf, gpistr, gpistr2
     
     printf_ty = ir.FunctionType(ir.IntType(32), [voidptr_ty], var_arg=True)
     printf = ir.Function(module, printf_ty, name="printf")
     function.functions["__printf"] = [printf,'void']
 
-    fmt = "%i \n\0"
+    fmt = "%i\n\0"
     c_fmt = ir.Constant(ir.ArrayType(ir.IntType(8), len(fmt)),
                         bytearray(fmt.encode("utf8")))
     gpistr = ir.GlobalVariable(module, c_fmt.type, name="fstr_int_n")
     gpistr.linkage = 'internal'
     gpistr.initializer = c_fmt  # type: ignore
     gpistr.global_constant = True
+
+    fmt = "%i\0"
+    c_fmt = ir.Constant(ir.ArrayType(ir.IntType(8), len(fmt)),
+                        bytearray(fmt.encode("utf8")))
+    gpistr2 = ir.GlobalVariable(module, c_fmt.type, name="fstr_int")
+    gpistr2.linkage = 'internal'
+    gpistr2.initializer = c_fmt  # type: ignore
+    gpistr2.global_constant = True
 
 def declare_exit(module):
     global exit_func
@@ -40,6 +49,12 @@ def declare_exit(module):
 def std_println_int(func, args):
     x = args[0]
     pistr = func.builder.bitcast(gpistr, voidptr_ty)
+    return func.builder.call(printf, [pistr, x])
+
+@function.internal_function("print", Ast_Types.Integer_32(), (Ast_Types.Integer_32(),))
+def std_print_int(func, args):
+    x = args[0]
+    pistr = func.builder.bitcast(gpistr2, voidptr_ty)
     return func.builder.call(printf, [pistr, x])
 
 @function.internal_function("exit", Ast_Types.Void(), (Ast_Types.Integer_32(),))
