7aff602a2120affb24c6888d65af502ea756143e
Benjamin Austin Jr <spidertyler1122@gmail.com>
2023-02-03 17:40:47 -0500

    experimental --emit-binary

--- src/Ast/module.py
+++ src/Ast/module.py
@@ -1,9 +1,9 @@
 import parser
 from typing import Self
 
-import errors
 from llvmlite import binding, ir
 
+import errors
 from Ast.function import _Function
 from Ast.nodes import ASTNode, SrcPosition
 
@@ -72,8 +72,8 @@ class Module(ASTNode):
         for child in self.children:
             child.value.eval()
 
-    def save_ir(self, loc, create_object_file = False):
-        target = self.target.create_target_machine()
+    def save_ir(self, loc, args = {}):
+        target = self.target.create_target_machine(force_elf=True, codemodel="default")
         module_pass = binding.ModulePassManager()
         # * commented out optimizations may be re-added later on
         pass_manager = binding.PassManagerBuilder()
@@ -98,10 +98,26 @@ class Module(ASTNode):
 
         with open(f"{loc}.ll", 'w') as output_file:
             output_file.write(str(mod))
-        if not create_object_file:
+        if (not args["--emit-object"]) and (not args["--emit-binary"]):
             return
         with open(f"{loc}.o", 'wb') as output_file:            
             output_file.write(target.emit_object(mod))
+        if not args["--emit-binary"]:
+            return
+        import os
+        binding.lld.lld_linux(f"{loc}",
+            [
+                "/usr/lib64/crt1.o", "/usr/lib64/crti.o",
+                "/usr/lib64/gcc/x86_64-pc-linux-gnu/12.2.1/crtbegin.o", f"{loc}.o",
+                "/usr/lib64/gcc/x86_64-pc-linux-gnu/12.2.1/crtend.o",
+                "/usr/lib64/crtn.o"
+            ], 
+            [
+                "-Bdynamic", "-no-pie", "--build-id", "--dynamic-linker",
+                "/lib64/ld-linux-x86-64.so.2", "-L/usr/lib/gcc/x86_64-pc-linux-gnu/12.2.1/",
+                "-L/usr/lib/", "-L/usr/lib64/", "-L/lib/", "-L/lib64/", "-lc"
+            ]
+        )
 
     def syntax_error_information(self, child, c: int):
         '''more useful syntax error messages'''
--- src/compile.py
+++ src/compile.py
@@ -3,6 +3,7 @@ import sys
 from contextlib import contextmanager
 from pathlib import Path
 from time import perf_counter
+from typing import Union
 
 import errors
 from errors import _print_raw, _print_text, inline_warning
@@ -16,7 +17,7 @@ def timingContext(text: str):
     _print_text(f'{text} in {perf_counter() - start} seconds{errors.RESET}')
 
 
-def compile(src_str: str, output_loc: str, create_object_file):
+def compile(src_str: str, output_loc: str, args):
     start = perf_counter()
 
     inline_warning("Python has notoriusly high memory usage, this applies for this compiler!\nThis compiler is written in python with llvmlite!")
@@ -40,17 +41,17 @@ def compile(src_str: str, output_loc: str, create_object_file):
 
     with timingContext('lexing finished'):
         tokens = lex.Lexer().get_lexer().lex(src_str)
-    
+
     with timingContext('parsing finished'):
         module = Ast.module.Module((-1,-1,-1), "main", src_str, tokens)
         Ast.standardfunctions.declare_all(module.module)
         module.parse()
-        
+
     with timingContext('module created'):
         module.pre_eval()
         module.eval()
-    
-    module.save_ir(output_loc, create_object_file = create_object_file)
+
+    module.save_ir(output_loc, args = args)
     code_gen.shutdown()
 
     _print_raw(f'{errors.GREEN}| IR saved, compilation done | {perf_counter() - start}s')
@@ -63,8 +64,24 @@ def compile(src_str: str, output_loc: str, create_object_file):
 
     _print_raw('\n\n\n')
 
+def create_args_dict(args: list[str]) -> dict[str, bool|str]:
+    '''creates a dictionary of command-line arguments.'''
+    args_dict: dict[str, bool|str] = { # contains all valid command line arguments
+            "--emit-object": False,
+            "--emit-binary": False
+        }
+    for arg in args:
+        if not arg.startswith('-'):
+            continue
+        if '=' not in arg:
+            args_dict[arg] = not args_dict[arg] # invert current value
+        else:
+            name, value = arg.split('=')
+            args_dict[name] = value
+    return args_dict
+
 def compile_file(file: Path, args):
     errors.FILE = str(file)
     with file.open() as f:
-        create_object_file = "--emit-object" in args
-        compile(f.read(), str(file.absolute().parents[0] / "output"), create_object_file)
+        args = create_args_dict(args)
+        compile(f.read(), str(file.absolute().parents[0] / "output"), args)
